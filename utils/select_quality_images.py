976000a22abe56fe0d4e5c04598591dcf875af09
Biagio <biagio.brattoli@iwr.uni-heidelberg.de>
2017-04-19 14:20:52 +0200

    utils quality selection improved

--- utils/select_quality_images.py
+++ utils/select_quality_images.py
@@ -1,8 +1,13 @@
 import csv
+import numpy as np
+import os
+
+files = os.listdir('images')
 
 csvfile = open('../weed_list_2017_02_23.csv', 'rb')
 spamreader = csv.reader(csvfile, delimiter=';', quotechar='|')
 
+names = []
 quality = []
 for row in spamreader:
     qlt = row[7]
@@ -11,12 +16,16 @@ for row in spamreader:
     else:
         qlt = row[8]
         name = row[13]
-    if qlt=='1' and name[1]=='R':
-        quality.append(name)
+    if name in files:
+        quality.append(int(qlt))
+        names.append(name)
 
+quality = np.asarray(quality)
+names = np.asarray(names)
+names = names[quality==1]
 
 out = open('unlabeled_quality1.txt','w')
-for name in quality:
+for name in names:
     out.write(name+'\n')
 
 out.close()
